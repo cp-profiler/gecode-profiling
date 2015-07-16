aba9b5003be3628f08fecab9bafbc4cc50ddf2f6
Chris Mears <chris@cmears.id.au>
2015-07-16 10:55:52 +1000

    Handle interrupts in ZMQ sending

--- gecode/search/connector.cpp
+++ gecode/search/connector.cpp
@@ -18,11 +18,30 @@ inline void Connector::sendOverSocket(message::Node &msg) {
   zmq::message_t request(msg_str.size());
   memcpy((void*)request.data(), msg_str.c_str(), msg_str.size());
 
-  int ne = socket.send(request);
-
-  if (ne == -1) {
-    std::cerr << "error while sending over socket\n";
-    abort();
+  // Sometimes sending will fail with EINTR.  In this case, we try to
+  // send the message again.
+  while (true) {
+    int failed_attempts = 0;
+    try {
+      bool sentOK = socket.send(request);
+      // If sentOK is false, there was an EAGAIN.  We handle this the
+      // same as EINTR.
+      if (!sentOK) {
+        failed_attempts++;
+        if (failed_attempts > 10) abort();
+        continue;
+      }
+      // Success: stop the loop.
+      break;
+    } catch (zmq::error_t &e) {
+      failed_attempts++;
+      if (failed_attempts > 10) abort();
+      if (e.num() == EINTR) {
+        continue;
+      }
+      // If it was something other than EINTR, rethrow the exception.
+      throw e;
+    }
   }
 }
 
